e6b5207b5669e099fdca0b2b69dfb74ddabe975d
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-11-27 13:38:00 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@ type=$1
 rclone_key=$2
 
 if [ $type = "one" ]; then
-   wget -q https://raw.github.com/zhlhlf/text/main/onedrive_mount.sh && sh onedrive_mount.sh $rclone_key 临时存放文件         
+   curl -sL https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/upload.sh | bash -s $rclone_key tyy2/临时存放文件
 elif [ $type = "wet" ];then
    wget https://raw.githubusercontent.com/zhlhlf/text/main/upload-wetransfer.py
    python upload-wetransfer.py 666/*
