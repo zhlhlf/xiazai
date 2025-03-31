3aee4d5860cf2d5dacb0877d0524cc085f1268b3
zhlhlf <zhlhlf@gmail.com>
2025-03-31 18:14:22 +0800

    asd

--- download.sh
+++ download.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-
 mkdir -p 666
 cd 666
 
-for i in $1;do
-    if [ `echo "$i" | grep "google.com/download"` ];then
-        rm -rf dGoogleUrl.sh; wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/dGoogleUrl.sh ; chmod 777 * -R
-        i=`./dGoogleUrl.sh "$i"`
+y_weblist="pornhub.com www.xvideos.com"
+
+for i in $1; do
+    if [ $(echo "$i" | grep "google.com/download") ]; then
+        rm -rf dGoogleUrl.sh
+        wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/dGoogleUrl.sh
+        chmod 777 * -R
+        i=$(./dGoogleUrl.sh "$i")
         echo $i
-    elif [ `echo "$i" | grep "pornhub.com"` ];then
-        ../youtube-dl.sh "$i" 0
-        continue
-    elif [ `echo "$i" | grep "www.xvideos.com"` ];then
-        youtube_dl=1
-        ../youtube-dl.sh "$i" 1
-        continue
+    elif [ "1" ]; then
+        yes=0
+        for u in $y_weblist; do
+            if [ $(echo $i | grep $u) ]; then
+                ../youtube-dl.sh "$i"
+                yes=1
+                break
+            fi
+        done
+        if [ "$yes" = "1" ]; then
+            continue
+        fi
     fi
     echo "UA: $3"
     aria2c -x$2 -U "$3" "$i"
 
 done
-
--- youtube-dl.sh
+++ youtube-dl.sh
@@ -3,20 +3,7 @@ i="$1"
 
 sudo pip install youtube-dl >>/dev/null
 
-mode="$2" # 第二个参数是模式
-
-if [ "$mode" = "0" ]; then
-    # 获取名字
-    name=$(curl -sL "$i" | grep inlineFree | cut -d'>' -f 2 | cut -d'<' -f 1)
-
-    # 可选的 name 限制（如果需要）
-    # name=$(echo "$name" | perl -CSD -pe 's/[^\x{4E00}-\x{9FFF}\x{3040}-\x{309F}\x{30A0}-\x{30FF}a-zA-Z0-9 ]//g')
-    # name="${name:0:60}"
-
-elif [ "$mode" = "1" ]; then
-    name=$(curl -sL "$i" | grep html5player.setVideoTitle | awk -F\' '{print $2}')
-fi
-
+name=$(curl -sL "$i" | grep '<title>' | cut -d\> -f 2 | awk -F- '{for(i=1;i<NF;i++) printf "%s%s", $i, (i==NF-1?"":FS)}')
 name=$(echo "$name" | sed 's#/#-#g')
 
 youtube-dl -o "$name.%(ext)s" "$i" --external-downloader aria2c --external-downloader-args "-x10"
