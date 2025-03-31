58f22484ff89524f026e230b4e4f6086ce395204
zhlhlf <zhlhlf@gmail.com>
2025-03-31 12:39:50 +0800

    asd

--- d_pronhub.sh
+++ d_pronhub.sh
@@ -9,9 +9,9 @@ do
     #获取名字
     name=`curl -sL $i | grep inlineFree | cut -d'>' -f 2 | cut -d'<' -f 1`
     #name限制
-    name=$(echo "$name" | perl -CSD -pe 's/[^\x{4E00}-\x{9FFF}\x{3040}-\x{309F}\x{30A0}-\x{30FF}a-zA-Z0-9 ]//g')
-    #限制长度为60
-    name="${name:0:60}"
+    # name=$(echo "$name" | perl -CSD -pe 's/[^\x{4E00}-\x{9FFF}\x{3040}-\x{309F}\x{30A0}-\x{30FF}a-zA-Z0-9 ]//g')
+    # #限制长度为60
+    # name="${name:0:60}"
   
     youtube-dl -o "$name.%(ext)s" $i --external-downloader aria2c --external-downloader-args "-x10" 
     echo $name
