ad007d6fbf0ef3a0db7898b3e8a9d522a050d683
zhlhlf <zhlhlf@gmail.com>
2025-04-01 02:22:50 +0800

    ads

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -4,8 +4,7 @@ i="$1"
 sudo pip install youtube-dl >>/dev/null
 
 name=$(curl -sL "$i" | grep '<title>' | cut -d\> -f 2 | awk -F- '{for(i=1;i<NF;i++) printf "%s%s", $i, (i==NF-1?"":FS)}')
-name=$(echo "$name" | sed 's#/#-#g')
-name=$(echo "$name" | rev | tr -d ' ' | rev)
+name=$(echo "$name" | sed 's#/#-#g' | sed 's/[[:space:]]*$//')
 
 youtube-dl -o "$name.%(ext)s" "$i" --external-downloader aria2c --external-downloader-args "-x10"
 echo "$name"
