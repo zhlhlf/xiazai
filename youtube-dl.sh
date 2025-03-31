84ad3374f823216d0b95093ed0493975fef8aa2b
zhlhlf <zhlhlf@gmail.com>
2025-03-31 17:51:49 +0800

    asd

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -13,11 +13,11 @@ if [ "$mode" = "0" ]; then
     # name=$(echo "$name" | perl -CSD -pe 's/[^\x{4E00}-\x{9FFF}\x{3040}-\x{309F}\x{30A0}-\x{30FF}a-zA-Z0-9 ]//g')
     # name="${name:0:60}"
 
-    # 使用获取的名字下载
-    youtube-dl -o "$name.%(ext)s" "$i" --external-downloader aria2c --external-downloader-args "-x10"
-    echo "$name"
-
 elif [ "$mode" = "1" ]; then
-    # 使用 youtube-dl 的默认标题下载
-    youtube-dl -o "%(title)s.%(ext)s" "$i" --external-downloader aria2c --external-downloader-args "-x10"
+    name=$(curl -sL "$i" | grep html5player.setVideoTitle | awk -F\' '{print $2}')
 fi
+
+name=` echo "$name" | sed 's#/#-#g'`
+
+youtube-dl -o "%(title)s.%(ext)s" "$i" --external-downloader aria2c --external-downloader-args "-x10"
+echo "$name"
