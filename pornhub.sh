e60c44ff83248a84e194fcfd594cd9b33cf79aa9
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2025-04-15 01:25:10 +0800

    Update pornhub.sh

--- pornhub.sh
+++ pornhub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 i="$1"
 
-sudo apt install ffmpeg >>/dev/null
+sudo apt install youtube-dl >>/dev/null 2>&1
 
 name=$(curl -sL "$i" | grep '<title>' | cut -d\> -f 2 | awk -F- '{for(i=1;i<NF;i++) printf "%s%s", $i, (i==NF-1?"":FS)}')
 name=$(echo "$name" | sed 's#/#-#g' | sed 's/[[:space:]]*$//')
