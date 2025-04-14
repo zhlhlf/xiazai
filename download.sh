884fa5c5c749b9849bbe50dce3a527be3ec3a792
Your Name <you@example.com>
2025-04-14 23:35:20 +0800

    新增加下载xvideos.com最高等级视频

--- download.sh
+++ download.sh
@@ -3,8 +3,6 @@
 mkdir -p 666
 cd 666
 
-y_weblist="pornhub.com www.xvideos.com"
-
 for i in $1; do
     if [ $(echo "$i" | grep "google.com/download") ]; then
         rm -rf dGoogleUrl.sh
@@ -12,18 +10,12 @@ for i in $1; do
         chmod 777 * -R
         i=$(./dGoogleUrl.sh "$i")
         echo $i
-    elif [ "1" ]; then
-        yes=0
-        for u in $y_weblist; do
-            if [ $(echo $i | grep $u) ]; then
-                ../youtube-dl.sh "$i"
-                yes=1
-                break
-            fi
-        done
-        if [ "$yes" = "1" ]; then
-            continue
-        fi
+    elif [ $(echo "$i" | grep ""pornhub.com") ]; then
+        ../pornhub.sh "$i"
+        continue
+    elif [ $(echo "$i" | grep ""www.xvideos.com") ]; then
+        ../xvideos.sh "$i"
+        continue
     fi
     echo "UA: $3"
     aria2c -x$2 -U "$3" "$i"
--- pronhub.sh
+++ pronhub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 i="$1"
 
-sudo pip install youtube-dl >>/dev/null
+sudo apt install ffmpeg >>/dev/null
 
 name=$(curl -sL "$i" | grep '<title>' | cut -d\> -f 2 | awk -F- '{for(i=1;i<NF;i++) printf "%s%s", $i, (i==NF-1?"":FS)}')
 name=$(echo "$name" | sed 's#/#-#g' | sed 's/[[:space:]]*$//')
