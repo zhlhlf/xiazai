666f6889b4e58b7996a3c17f1ed238d099ef474e
Your Name <you@example.com>
2025-05-21 20:12:18 +0800

    asd

--- download.sh
+++ download.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+logfile="$pwd/dl.log"
+
 mkdir -p 666
 cd 666
 
+touch $logfile
+
+asd(){
+    while true;do
+        sleep 5
+        tail -n 1 $logfile
+        echo
+    done
+}
+
+asd &
+
+out_pid=$!
+
+current=0
+total_params=$(echo "$1" | wc -w)
+
 for i in $1; do
+
+    ((current++))
+    echo "Processing item $current/$total_params: $i"
+
     if [ $(echo "$i" | grep "google.com/download") ]; then
         rm -rf dGoogleUrl.sh
         wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/dGoogleUrl.sh
@@ -18,15 +41,17 @@ for i in $1; do
         ./download.sh "$u_list"
         continue
     elif [ $(echo "$i" | grep "pornhub.com") ]; then
-        ../pornhub.sh "$i"
+        ../pornhub.sh "$i" "$logfile"
         continue
     elif [ $(echo "$i" | grep "www.xvideos.com") ]; then
         ../xvideos.sh "$i"
         continue
     fi
     echo "UA: $3"
-    aria2c -x$2 -U "$3" "$i"
+    aria2c -x$2 -U "$3" "$i" >>$logfile 2>&1
 
 done
 
+kill $out_pid
 rm -rf $logfile
+
--- pornhub.sh
+++ pornhub.sh
@@ -5,6 +5,6 @@ pip install youtube-dl >>/dev/null 2>&1
 
 name=$(curl -sL "$i" | grep '<title>' | cut -d\> -f 2 | awk -F- '{for(i=1;i<NF;i++) printf "%s%s", $i, (i==NF-1?"":FS)}')
 name=$(echo "$name" | sed 's#/#-#g' | sed 's/[[:space:]]*$//')
-
-youtube-dl -o "%(uploader)s - $name.%(ext)s" "$i" --external-downloader aria2c --external-downloader-args "-x10"
 echo "$name"
+
+youtube-dl -o "%(uploader)s - $name.%(ext)s" "$i" --external-downloader aria2c --external-downloader-args "-x10" >> $2 2>&1
