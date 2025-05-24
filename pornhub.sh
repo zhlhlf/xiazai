3e5b2f4b0cdbeb354d5414667fb568e1ceaa7c3c
Your Name <you@example.com>
2025-05-25 03:39:03 +0800

    as

--- pornhub.sh
+++ pornhub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 i="$1"
 
-pip install youtube-dl >>/dev/null 2>&1 
+sudo apt install yt-dlp ffmpeg >>/dev/null 2>&1 
 
 name=$(curl -sL "$i" | grep '<title>' | cut -d\> -f 2 | awk -F- '{for(i=1;i<NF;i++) printf "%s%s", $i, (i==NF-1?"":FS)}')
 name=$(echo "$name" | sed 's#/#-#g' | sed 's/[[:space:]]*$//')
